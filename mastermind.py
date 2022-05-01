6ba0379273ceb27fb8d1a6f4094f9bbff97050db
Peter Lawson <lawsonpd@pm.me>
2022-05-01 17:54:29 -0500

    Add more tests. Fix assertion statement.

--- mastermind.py
+++ mastermind.py
@@ -7,7 +7,7 @@ class Mastermind:
 
     def make_guess(self, guess):
         "Add guess (str) to self.guesses and return clue if not last guess, otherwise end game."
-        assert(not self.gameover, "Game is over") # Necessary?
+        assert not self.gameover, "Game is over" # Necessary?
         guess = guess.split()
         self._record_guess(guess)
         clue = self._evaluate_guess(guess)
@@ -58,9 +58,23 @@ class Mastermind:
 
 def test():
     mm = Mastermind()
+
+    # Manually set secret code. Otherwise won't know what it is.
     mm.secret = ['red', 'red', 'purple', 'red']
-    clue = mm.make_guess('purple red red red'.split())
+    clue = mm.make_guess('purple red red red')
     assert clue.sort() == ['black', 'black', 'white', 'white']
+    assert not mm.gameover
+    # Make 9 more (incorrect) guesses and check that game is over
+    incorrect_guess = 'blue blue blue blue'
+    for _ in range(9):
+        mm.make_guess(incorrect_guess)
+    assert mm.gaveover
+    assert len(mm.guesses) == len(mm.clues)
+
+    mm = Mastermind()
+    mm.secret = ['red', 'red', 'purple', 'red']
+    clue = mm.make_guess('red red purple red')
+    assert mm.gaveover
 
 def game():
     mm = Mastermind()
