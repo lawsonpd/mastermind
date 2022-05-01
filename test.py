b7b2e3de02326f526563124a277955aa4ad8e8cb
Peter Lawson <lawsonpd@pm.me>
2022-05-01 18:28:01 -0500

    Add more tests (using unittest). Move files back into top-level dir.

--- test.py
+++ test.py
@@ -1,22 +1,39 @@
+import unittest
 from mastermind import Mastermind
 
-def test():
-    mm = Mastermind()
+class TestMastermind(unittest.TestCase):
+    def test_clue_validity(self):
+        "Tests that correct clue is given."
+        mm = Mastermind()
+        # Manually set secret code. Otherwise won't know what it is.
+        mm.secret = ['red', 'red', 'purple', 'red']
+        clue = mm.make_guess('purple red red red')
+        clue.sort()
+        assert clue == ['black', 'black', 'white', 'white']
 
-    # Manually set secret code. Otherwise won't know what it is.
-    mm.secret = ['red', 'red', 'purple', 'red']
-    clue = mm.make_guess('purple red red red')
-    assert clue.sort() == ['black', 'black', 'white', 'white']
-    assert not mm.gameover
-    # Make 9 more (incorrect) guesses and check that game is over
-    incorrect_guess = 'blue blue blue blue'
-    for _ in range(9):
+    def test_not_gameover_after_one_incorrect_guess(self):
+        "tests that mm.gameover == False after a single incorrect guess."
+        mm = Mastermind()
+        mm.secret = ['red', 'red', 'purple', 'red']
+        incorrect_guess = 'blue blue blue blue'
         mm.make_guess(incorrect_guess)
-    assert mm.gaveover
-    assert len(mm.guesses) == len(mm.clues)
+        assert not mm.gameover
 
-    mm = Mastermind()
-    mm.secret = ['red', 'red', 'purple', 'red']
-    clue = mm.make_guess('red red purple red')
-    assert mm.gaveover
+    def test_gameover_after_10_guesses(self):
+        "Tests that mm.gameover == True when 10 guess have been made."
+        mm = Mastermind()
+        mm.secret = ['red', 'red', 'purple', 'red']
+        incorrect_guess = 'blue blue blue blue'
+        for _ in range(10):
+            mm.make_guess(incorrect_guess)
+        assert mm.gameover
 
+    def test_gameover_after_correct_guess(self):
+        "Tests that mm.gameover == True when a correct guess has been made."
+        mm = Mastermind()
+        mm.secret = ['red', 'red', 'purple', 'red']
+        clue = mm.make_guess('red red purple red')
+        assert mm.gameover
+
+if __name__ == '__main__':
+    unittest.main()
